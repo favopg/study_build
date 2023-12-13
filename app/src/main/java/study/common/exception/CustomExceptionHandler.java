4328b5e5037415c5d187d5a562d1bb489db0dd17
favopg <favoriteigo0904@gmail.com>
2023-12-13 21:05:40 +0900

    エラーハンドリングをおためし中

---
+++ app/src/main/java/study/common/exception/CustomExceptionHandler.java
@@ -0,0 +1,18 @@
+package study.common.exception;
+
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class CustomExceptionHandler {
+	
+	@ExceptionHandler(Exception.class)
+	public String test(Exception ex, Model model) {
+		System.out.println(ex.getMessage());
+		System.out.println(ex.getMessage());
+		
+		return "error/error";
+	}
+
+}
