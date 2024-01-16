8a91af4d04f7e0b69efd3f264368bdc4e1de8368
favopg <favoriteigo0904@gmail.com>
2024-01-16 15:41:48 +0900

    '登録時のユーザ情報修正'

--- app/src/main/java/study/common/CustomAuditorAware.java
+++ app/src/main/java/study/common/CustomAuditorAware.java
@@ -2,19 +2,25 @@ package study.common;
 
 import java.util.Optional;
 
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.AuditorAware;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 
 public class CustomAuditorAware implements AuditorAware<String>{
-
+	
+	@Autowired
+	HttpSession session;
+	
 	@Override
 	public Optional<String> getCurrentAuditor() {
-		
+				
 		Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
 		
 		if("新規ユーザ".equals(authentication.getName())) {
-			return null;
+			return Optional.of((String)session.getAttribute("userName"));
 		}
 				
 		return Optional.of(((UserDetail) authentication.getPrincipal()).getUsername());
